407672b8666675ccbf613efbb6d2d8f5ee1f6c89
chinnmayk <chinnmayk@gmail.com>
2026-02-03 16:02:13 +0530

    ECS Cluster + IAM

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -13,3 +13,11 @@ output "security_group_id" {
 output "ecr_repository_url" {
   value = aws_ecr_repository.app.repository_url
 }
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
+
+output "ecs_execution_role_arn" {
+  value = aws_iam_role.ecs_execution.arn
+}
